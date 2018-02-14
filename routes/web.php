5ef0018d13dc380eb0cb1b4424e7ef6d1e80464e
Mateus Esteves <mateus.esteves1994@gmail.com>
2018-02-14 19:47:48 -0200

    Routing and Auth sections complete

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/tasks', 'TaskController@index');
+Route::post('/task', 'TaskController@add');
+Route::delete('/task/{id}', 'TaskController@delete');
