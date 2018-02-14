81529334f8b41124b09fa12a59f7c2ab7e4547fe
Mateus Esteves <mateus.esteves1994@gmail.com>
2018-02-14 18:33:49 -0200

    Section 'Prepping the database' complete

--- app/User.php
+++ app/User.php
@@ -4,26 +4,32 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Task;
 
 class User extends Authenticatable
 {
     use Notifiable;
-
+    
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email',
     ];
-
+    
     /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+    * The attributes that should be hidden for arrays.
+    *
+    * @var array
+    */
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);    
+    }
 }
